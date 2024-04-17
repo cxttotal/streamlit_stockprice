9e17e09324936f41abf3186839e68549ad649493
cxttotal <37193165+cxttotal@users.noreply.github.com>
2024-04-17 15:14:28 +0800

    Add files via upload

---
+++ stockprice_API.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import tushare as ts
+
+ts.set_token(st.secrets["auth_token"])
+pro = ts.pro_api()
+
+tickerSymbol = '000001.SZ'
+tickerDf = pro.daily(ts_code=tickerSymbol, start_date='20180701', end_date='20180718')
+tickerDf = tickerDf.sort_values(by='trade_date', ascending=True)
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock **closing price** and ***volume*** of 000001.SZ!
+
+""")
+
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf.close)
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.vol)
